19bae8429f129905275b438d3c544548530546e8
ReDetection <redetection@gmail.com>
2015-01-11 02:26:31 +0600

    some cleanup

--- powerdowntest.ino
+++ powerdowntest.ino
@@ -1,15 +1,11 @@
-// **** INCLUDES *****
 #include "LowPower.h"
+
 #define LED_PIN 3
 
-void wakeUp() {
-      // Disable external pin interrupt on wake up pin.
-//      detachInterrupt(0); 
-}
+void wakeUp() {}
 
-void setup()
-{
-  pinMode(LED_PIN, OUTPUT);
+void setup() {
+    pinMode(LED_PIN, OUTPUT);
   
     digitalWrite(LED_PIN, HIGH);
     delay(50);
@@ -18,25 +14,17 @@ void setup()
     digitalWrite(LED_PIN, HIGH);
     delay(50);
     digitalWrite(LED_PIN, LOW);
-    
-    // Configure wake up pin as input.
-    // This will consumes few uA of current.
+
     pinMode(2, INPUT);  
     delay(20);
-   // Allow wake up pin to trigger interrupt on low.
+
     attachInterrupt(0, wakeUp, CHANGE);
-     
 }
 
-void loop() 
-{
-    
-    // Enter power down state with ADC and BOD module disabled.
-    // Wake up when wake up pin is low.
-    LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
-    
-    
-    
+void loop() {
+    LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+
+
     digitalWrite(LED_PIN, HIGH);
     delay(1000);
     digitalWrite(LED_PIN, LOW);
