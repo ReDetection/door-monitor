222e57f567bc6e68ff15923e530fc67f8f2472bb
ReDetection <redetection@gmail.com>
2015-01-17 11:57:09 +0600

    no message

--- powerdowntest.ino
+++ powerdowntest.ino
@@ -1,13 +1,54 @@
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
 #include "LowPower.h"
 
 #define LED_PIN 3
 #define DOOR_PIN 2
 
+RF24 radio(9,10);
+const uint64_t dstAddress = 0xE8E8F5F0E1LL;
+const uint64_t nodeAddress = 0xE8E8F5FCA1LL;
+
+struct sMessage {
+  unsigned long time;
+  uint8_t doorIsClosed : 1;
+  uint8_t doorIsLocked : 1;
+  uint8_t              : 6;
+};
+typedef struct sMessage Message;
+
 void wakeUp() {}
 
-void sendState(bool open) {
+void sendState(bool closed) {
     digitalWrite(LED_PIN, HIGH);
-    delay(1000);
+    delay(10);
+    
+    Message message;
+    message.time = millis();
+    message.doorIsClosed = closed ? 1 : 0;
+    message.doorIsLocked = 0;
+    
+    bool success = false;
+    while (!success) {
+      radio.write(&message, sizeof(Message));
+    
+      unsigned long started_waiting_at = millis();
+      bool timeout = false;
+      while ( ! radio.available() && ! timeout )
+        if (millis() - started_waiting_at > 100 )
+          timeout = true;
+
+      if (timeout) {
+        printf("Failed, response timed out.\n\r");
+      
+      } else {
+        unsigned long ackMessageWithTime;
+        radio.read( &ackMessageWithTime, sizeof(unsigned long) );
+        success = ackMessageWithTime == message.time;
+      }
+    }
+    
     digitalWrite(LED_PIN, LOW);
     delay(10);
 }
@@ -29,6 +70,10 @@ void setup() {
 
     pinMode(DOOR_PIN, INPUT);
     delay(20);
+    
+    radio.begin();
+    radio.openWritingPipe(dstAddress);
+    radio.openReadingPipe(1,nodeAddress);
 
     sendState(true);
 
