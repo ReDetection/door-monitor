31a09f353c4b3d7813697903dd9f92e1d42e81ac
ReDetection <redetection@gmail.com>
2015-02-03 00:50:24 +0600

    basic daemon with redis storage

---
+++ server-daemon/daemon.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import serial
+import json
+import redis as redislib
+import time
+
+arduino = serial.Serial('/dev/tty.usbmodem1411', 57600) # todo: commandline parameter
+redis = redislib.StrictRedis(host='192.168.12.20', port=6379, db=5) #todo config
+
+while 1:
+	line = arduino.readline()
+	print line 
+
+	message = json.loads(line)
+
+	key = time.strftime("%d-%m-%Y") + "-" + str(message["nodeMillis"]) + "-" + ("closed" if message["doorClosed"] else "open")
+	redis.set(key, message)
+
+
+
+
+
+
